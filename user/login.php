4ab5c00c69e352c9d4578f1478deb5cfd869a6c1
Jenny <jenypau@gmail.com>
2024-11-27 19:01:05 +0100

    actualizacion

--- user/login.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-$_SESSION['user'] = $_GET['user'];
-$url = '..?op=login&result=ok';
-header('Location: ' . $url);
--- user/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['user']);
-$url = '..?op=logout&result=ok';
-header('Location: ' . $url);
